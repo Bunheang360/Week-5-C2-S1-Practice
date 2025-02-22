766df623e5b7d126983b87d46cd70d05877018e0
Chheng Bunheang <2005chhengbunheang@gmail.com>
2025-02-22 13:41:40 +0700

    Add files via upload

---
+++ exercise-2/exercise2/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header'; // Correct path
+import Body from './Body'; // Assuming Main is another component
+import Footer from './Footer'; // Assuming Footer is another component
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body/>
+      <Footer />
+  </>
+  );
+}
+
+export default App;
